85ad1173ded240b23dcca0816a878042c8343655
Chrissy LeMaire <clemaire@gmail.com>
2021-06-28 00:47:21 +0200

    Update twitter.ps1

--- twitter/twitter.ps1
+++ twitter/twitter.ps1
@@ -155,7 +155,9 @@ foreach ($mention in $mentions) {
         
         # if anyone else on the thread is blocked
         foreach ($entity in $mention.Entities.UserName) {
-            $id = (Get-TwitterUser -UserName $entity).Id
+            try {
+                $id = (Get-TwitterUser -UserName $entity).Id
+            } catch {}
             if ($id -in $blocked.Id) {
                 Write-Warning "BLOCKING $(author.UserName) FOR RELATED BLOCKS ($id)"
                 #Set-TwitterBlockedUser -User $author.UserName -Block
